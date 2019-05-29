28dfbbf2db66c5a472adcce2d74ff01dd5a3bade
nalakaJ <nalakae06105@gmail.com>
2019-05-29 15:02:28 +0530

    Slider implimentation

--- src/app/AppModule.ts
+++ src/app/AppModule.ts
@@ -1,27 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { HomeComponent } from './home/home.component';
-import { SlideshowModule} from 'ng-simple-slideshow';
-
+import { SlideshowModule } from 'ng-simple-slideshow';
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
     ContactsComponent,
-    HomeComponent
-    
+    HomeComponent,
+    SlideshowModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SlideshowModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
